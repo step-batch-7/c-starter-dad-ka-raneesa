bc1543245de79dc1f799767cf80d6dfbafa58b66
Deepika <bhuvanadeepika017@gmail.com>
2020-03-28 22:50:44 +0530

    added average_of_three function to basic_functions

--- basic_functions.c
+++ basic_functions.c
@@ -7,12 +7,13 @@ long int square(int);
 long int cube(int);
 int gcd(int, int);
 int lcm(int, int);
-double simple_interest(double, int, double);
-double compound_interest(double, int, double);
+double simple_interest(double, double, double);
+double compound_interest(double, double, double);
 double fahrenheit_to_centigrade(double);
 double centigrade_to_fahrenheit(double);
 double get_greatest(double, double);
 double greatest_of_three(double, double, double);
+double average_of_three(double, double, double);
 
 unsigned char is_even(int number)
 {
@@ -46,12 +47,12 @@ int lcm(int num1, int num2)
   return num1 * num2 / gcd(num1, num2);
 }
 
-double simple_interest(double p, int n, double r)
+double simple_interest(double p, double n, double r)
 {
   return (p * n * r) / 100;
 }
 
-double compound_interest(double p, int n, double r){
+double compound_interest(double p, double n, double r){
   return p* (pow((1+r/100),n)); 
 }
 
@@ -65,18 +66,25 @@ double centigrade_to_fahrenheit(double temperature)
   return (temperature * 9 / 5) + 32;
 }
 
-double get_greatest(double num1, double num2) {
+double get_greatest(double num1, double num2) 
+{
   return num1 > num2 ? num1 : num2;
 }
 
-double greatest_of_three(double num1, double num2, double num3) {
+double greatest_of_three(double num1, double num2, double num3) 
+{
   return get_greatest(get_greatest(num1, num2), num3);
+}
+
+double average_of_three(double num1, double num2, double num3)
+{
+  return (num1 + num2 + num3) / 3;
 }
 
 int main(void)
 {
-  int num1, num2, num3, time;
-  double num4, num5, num6, principle, rate, temperature;
+  int num1, num2, num3;
+  double num4, num5, num6, principle, rate, time, temperature;
   printf("Enter a number :\n");
   scanf("%d", &num1);
   printf("%d is %s\n", num1, is_even(num1) ? "even" : "not even");
@@ -88,7 +96,7 @@ int main(void)
   printf("G.C.D. of %d and %d is %d \n", num2, num3, gcd(num2, num3));
   printf("L.C.M. of %d and %d is %d \n", num2, num3, lcm(num2, num3));
   printf("\nEnter the principle amount,number of years, rate of interest to calculate simple and compound interest :\n");
-  scanf("%lf %d %lf", &principle, &time, &rate);
+  scanf("%lf %lf %lf", &principle, &time, &rate);
   printf("Simple interest is %lf \n", simple_interest(principle, time, rate));
   printf("Compound interest is %lf \n", compound_interest(principle, time, rate));
   printf("Enter fahrenheit temperature to convert into centigrade :\n");
@@ -97,7 +105,8 @@ int main(void)
   printf("Enter centigrade temperature to convert into fahrenheit :\n");
   scanf("%lf", &temperature);
   printf("fahrenheit of %f centigrade is %5.2lf\n", temperature, centigrade_to_fahrenheit(temperature));
-  printf("Enter three numbers to calculate greatest number :\n");
+  printf("Enter three numbers to calculate greatest number and average of three :\n");
   scanf("%lf %lf %lf", &num4, &num5, &num6);
-  printf("%lf is the greatest of %lf, %lf and %lf",greatest_of_three(num4, num5, num6), num4, num5, num6);
+  printf("%lf is the greatest of %lf, %lf and %lf\n",greatest_of_three(num4, num5, num6), num4, num5, num6);
+  printf("%lf is the average of %lf, %lf and %lf",average_of_three(num4, num5, num6), num4, num5, num6);
 }
