6326d8f1c3aec600f4d745881ec2f8cdbcc507ad
Deepika <bhuvanadeepika017@gmail.com>
2020-03-29 18:15:13 +0530

    added print_odds function to loops.c

--- loops.c
+++ loops.c
@@ -2,6 +2,8 @@
 
 unsigned long int factorial(int);
 int fibonacci(int);
+unsigned char is_even(int);
+int print_odds(int);
 
 unsigned long int factorial(int number){
   unsigned long int fact = 1;
@@ -23,6 +25,18 @@ int fibonacci(int length){
   return 0;
 }
 
+unsigned char is_even(int number) {
+  return number % 2 == 0 ;
+}
+
+int print_odds(int lastNum){
+  printf("Odd numbers between %d and %d are :\n", 1, lastNum);
+  for(int num = 1; num <= lastNum; num++){
+   !is_even(num) && printf("%d\n", num);
+  }
+  return 0;
+}
+
 
 int main(void){
   int num;
@@ -33,5 +47,9 @@ int main(void){
   printf("Enter a number to get fibonacci series :\n");
   scanf("%d", &num);
   fibonacci(num);
+
+  printf("Enter a number to print odds from 1 to that number :\n");
+  scanf("%d", &num);
+  print_odds(num);
   return 0;
 }
